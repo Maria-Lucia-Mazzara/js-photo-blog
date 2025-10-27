adecc204ac07bcc31703b5a43791da07236a9b52
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-27 18:34:04 +0100

    sistemato layout polaroid perchè le ho allineate in modo tale da metterle in centro, e ho sistemato anche i titoli delle polaroid in modo tale da farle allineare con l'immagine

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,19 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
 
         res.data.forEach(element_card => {
             const card_piena = `
-            <div class="card mt-4 ms-3 rounded-0" style="width: 18rem;">
-            <img class="card-img-top pt-3" src="${element_card.url}" alt="prima_card">
-            <div class="card-body">
-            <h4 class="date_card">${element_card.date}</h4>
-            <h2 class="title_card">${element_card.title}</h2>
-            </div>
-            </div>
+            <div class="card mt-4 ms-2 rounded-0 col-lg-3">
+                    <img class="card-img-top pt-3" src="${element_card.url}" alt="prima_card">
+                    <div class="card-body p-0 pt-4 pb-4">
+                        <h4 class="date_card">
+                            ${element_card.date}
+                        </h4>
+                        <h2 class="title_card">
+                            ${element_card.title}
+                        </h2>
+                    </div>
+                </div>
             `;
-
+            card.innerHTML += card_piena
         });
 
     })
