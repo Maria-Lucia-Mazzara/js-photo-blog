f8756f301fe2d69d6b0246f1154a06d44ff79e2c
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-27 13:28:36 +0100

    consol.log elementi della chiamata

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,13 @@ console.log(document);
 
 
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
+    .then(res => {
+        console.log(res.data.date);
+        console.log(res.data.title);
+        console.log(res.data.url);
+        
+    })
+    .catch(error => {
+        console.log(error.message);
+
+    });
