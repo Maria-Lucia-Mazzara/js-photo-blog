57c89ec6f9bd29c661d32e4e7592f775588d96a9
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-27 18:00:24 +0100

    ho reso la card una stringa in modo tale che l'html lo possa leggere perchè non accetta elementi dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,21 @@ const card = document.querySelector('.row')
 
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
     .then(res => {
-        console.log(res.data.date);
-        console.log(res.data.title);
-        console.log(res.data.url);
+        // console.log(res.data.date);
+        // console.log(res.data.title);
+        // console.log(res.data.url);
 
         res.data.forEach(element_card => {
-            card.innerHTML += 
+            const card_piena = `
             <div class="card mt-4 ms-3 rounded-0" style="width: 18rem;">
-                <img class="card-img-top pt-3" src="${element_card.url}" alt="prima_card">
-                <div class="card-body">
-                    <h4 class="date_card">${element_card.date}</h4>
-                    <h2 class="title_card">${element_card.title}</h2>
-                </div>
+            <img class="card-img-top pt-3" src="${element_card.url}" alt="prima_card">
+            <div class="card-body">
+            <h4 class="date_card">${element_card.date}</h4>
+            <h2 class="title_card">${element_card.title}</h2>
             </div>
-            
+            </div>
+            `;
+
         });
 
     })
