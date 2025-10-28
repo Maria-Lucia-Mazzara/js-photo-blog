cfd9541aac84a83c1d453ec19661a0b41109a01d
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-28 15:56:55 +0100

    ho preso la classe delle immagini della card, e ho richiamato la classe in js. poi ho fatto due foreach uno per aprire le immagini uno per chiuderlo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 console.log(document);
 
 const card = document.querySelector('.row');
+const overlay = document.querySelector('.nascosta');
+const tutte_img = document.querySelectorAll('.card_img');
+const close_btn = document.querySelector('.close-btn');
 
 
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
@@ -12,7 +15,7 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
         res.data.forEach(element_card => {
             const card_piena = `
             <div class="card mt-4 ms-2 rounded-0 col-lg-3">
-                    <img class="card-img-top pt-3" src="${element_card.url}" alt="prima_card">
+                    <img class="card_img card-img-top pt-3" src="${element_card.url}" alt="prima_card">
                     <div class="card-body p-0 pt-4 pb-4">
                         <h4 class="date_card">
                             ${element_card.date}
@@ -31,3 +34,14 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
         console.log(error.message);
 
     });
+
+
+
+tutte_img.forEach(img => {
+    img.addEventListener('click', function () {
+        overlay.classList.remove('nascosta');
+    });
+});
+close_btn.addEventListener('click', function () {
+    overlay.classList.add('nascosta');
+});
